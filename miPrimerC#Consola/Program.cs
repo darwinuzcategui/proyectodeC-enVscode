6c46095e24b2d8ec232541fd7b8cd4f1edc0fa4e
Darwin Uzcategui <darwin.uzcategui1973@hotmail.com>
2019-10-24 17:51:41 -0400

    prueba

---
+++ miPrimerC#Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace miPrimerC_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola darwin!");
+        }
+    }
+}
---
+++ resreqApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace resreqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=6";
+            var cliente = new WebClient();
+            var json = cliente.DownloadString(API_URL);
+            //Console.WriteLine(json);
+            dynamic postUsuarios = JsonConvert.DeserializeObject(json);
+            foreach (var publicacion in postUsuarios)
+            {
+                Console.WriteLine(publicacion.id+": "+publicacion.title);
+                
+            }
+        }
+    }
+}
